1258562fb1b7ab8ea4a59f27f8f887a1c86f6f42
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-11 07:04:50 +0900

    2 files modified

--- DatePicker.tsx
+++ DatePicker.tsx
@@ -1,76 +1,15 @@
-// import * as React from 'react';
-// import { useState, useRef } from 'react';
-// import DatePick, { registerLocale } from 'react-datepicker';
-// import 'react-datepicker/dist/react-datepicker.css';
-// import './DatePicker.css';
-
-// import { ko } from 'date-fns/esm/locale';
-// import getYear from 'date-fns/getYear';
-// import getMonth from 'date-fns/getMonth';
-
-// registerLocale('ko', ko);
-
-// const DatePicker = () => {
-//   const calendar = useRef(null);
-//   const [startDate, setStartDate] = useState(null);
-//   const [endDate, setEndDate] = useState(null);
-
-//   const onChange = (date) => {
-//     const [start, end] = date;
-//     setStartDate(start);
-//     setEndDate(end);
-//   };
-//   const [month, setMonth] = useState(new Date().getMonth());
-//   const [date, setDate] = useState(new Date());
-
-//   const handleMonthChange = (date) => {
-//     setMonth(date.getMonth());
-//   };
-//   return (
-//     <div>
-//       <div>
-//         <DatePick
-//           withPortal
-//           className="date date-record"
-//           locale="ko"
-//           dateFormat="yyyy.MM.dd(eee)"
-//           selected={startDate}
-//           // minDate={minDate}
-//           // maxDate={maxDate}
-//           selected={date}
-//           onChange={(selectDate) => setDate(selectDate)}
-//           onMonthChange={handleMonthChange}
-//           useWeekdaysShort={true}
-//           shouldCloseOnSelect={false}
-//           // excludeDates={excludeDates}
-//           ref={calendar}
-//           // onInputClick={() => openDatePicker()}
-//           // onChange={(date, event) => datePickHandler(date, event)}
-//           dayClassName={(d) =>
-//             d.getDate() === date.getDate()
-//               ? 'selected-day'
-//               : d.getMonth() === month
-//               ? 'custom-day'
-//               : 'custom-day gray-day'
-//           }
-//         >
-//           <div className="button-container">
-//             <div className="btn_ctrl btn_ctrl-cancel">취소</div>
-//             <div className="btn_ctrl btn_ctrl-confirm">선택</div>
-//           </div>
-//         </DatePick>
-//       </div>
-//     </div>
-//   );
-// };
-
-import React, { useState } from 'react';
-import { getYear } from 'date-fns'; // year을 표시하기 위함입니다.
-import DatePicker, { registerLocale } from 'react-datepicker';
+import * as React from 'react';
+import { useState, useRef } from 'react';
+import DatePick, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import ko from 'date-fns/locale/ko'; //한국어로
-registerLocale('ko', ko); //한국어로
+import './DatePicker.css';
+
+import { ko } from 'date-fns/esm/locale';
+import getYear from 'date-fns/getYear';
+import getMonth from 'date-fns/getMonth';
 const _ = require('lodash'); //_.range를 표현하기 위한 겁니다.
+
+registerLocale('ko', ko);
 
 const DatePicker = () => {
   const calendar = useRef(null);
@@ -88,39 +27,80 @@ const DatePicker = () => {
   const handleMonthChange = (date) => {
     setMonth(date.getMonth());
   };
+
+  const years = _.range(1990, getYear(new Date()) + 1, 1);
+  const months = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+  ]; //month표시
   return (
     <div>
-      <DatePicker
-        locale="ko"
-        showPopperArrow={false}
-        fixedHeight
-        selected={this.state.startDate}
-        onChange={(date) =>
-          this.setState({
-            startDate: date,
-          })
-        }
-        renderCustomHeader={({
-          date,
-          decreaseMonth,
-          increaseMonth,
-          prevMonthButtonDisabled,
-          nextMonthButtonDisabled,
-        }) => (
-          <div className="date-customheader">
-            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-              <i className="icon-arrow-left32" /> //icomoon 폰트입니다.
-            </button>
-            //select 안넣을 거니까 년,월 표시하는 것만 넣었습니다.
-            <div className="custom-month">
-              {date.getFullYear()}년 {months[date.getMonth()]}
+      <div>
+        <DatePick
+          withPortal
+          className="date date-record"
+          locale="ko"
+          dateFormat="yyyy.MM.dd(eee)"
+          selected={startDate}
+          // minDate={minDate}
+          // maxDate={maxDate}
+          selected={date}
+          onChange={(selectDate) => setDate(selectDate)}
+          onMonthChange={handleMonthChange}
+          useWeekdaysShort={true}
+          shouldCloseOnSelect={false}
+          // excludeDates={excludeDates}
+          ref={calendar}
+          inline={true}
+          // onInputClick={() => openDatePicker()}
+          // onChange={(date, event) => datePickHandler(date, event)}
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled,
+          }) => (
+            <div className="date-customheader">
+              <div>
+                {date.getFullYear()}. {months[date.getMonth()]}
+              </div>
+
+              <button
+                onClick={decreaseMonth}
+                disabled={prevMonthButtonDisabled}
+              >
+                <i className="icon-arrow-left32" />
+              </button>
+              {/* //select 안넣을 거니까 년,월 표시하는 것만 넣었습니다. */}
+              <div className="custom-month">
+                {date.getFullYear()}. {months[date.getMonth()]}
+              </div>
+              <button
+                onClick={increaseMonth}
+                disabled={nextMonthButtonDisabled}
+              >
+                <i className="icon-arrow-right32" />
+              </button>
             </div>
-            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-              <i className="icon-arrow-right32" /> //icomoon폰트입니다.
-            </button>
+          )}
+        >
+          <div className="button-container">
+            <div className="btn_ctrl btn_ctrl-cancel">취소</div>
+            <div className="btn_ctrl btn_ctrl-confirm">선택</div>
           </div>
-        )}
-      />
+        </DatePick>
+      </div>
     </div>
   );
 };
